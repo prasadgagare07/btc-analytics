4231c00ea6260e750de4f1272687b01ccae95a67
Prasad Gagare <prasadgagare91@gmail.com>
2026-07-15 11:35:30 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,24 @@
 
+async function loadStatus(){
+
+try{
+
+const res=await fetch("/api/status");
+
+const data=await res.json();
+
+document.getElementById("status").innerHTML=
+data.status;
+
+}
+
+catch{
+
+document.getElementById("status").innerHTML=
+"Server Offline";
+
+}
+
+}
+
+loadStatus();
