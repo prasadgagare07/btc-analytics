3fe595de81a810f31f2f914faa9714133d30504a
Prasad Gagare <prasadgagare91@gmail.com>
2026-07-15 11:45:31 +0530

    Create marketstate.js

---
+++ backend/data/marketstate.js
@@ -0,0 +1,16 @@
+const marketState = {
+    lastPrice: 0,
+
+    buyVolume: 0,
+    sellVolume: 0,
+
+    delta: 0,
+    cvd: 0,
+
+    tradeCount: 0,
+
+    largestBuy: 0,
+    largestSell: 0
+};
+
+module.exports = marketState;
