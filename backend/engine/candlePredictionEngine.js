657ba7e8251ee1d5c717263d86803c7cbeb99ad0
Prasad Gagare <prasadgagare91@gmail.com>
2026-07-20 16:22:46 +0530

    Update candlePredictionEngine.js

--- backend/engine/candlePredictionEngine.js
+++ backend/engine/candlePredictionEngine.js
@@ -8,7 +8,36 @@ function getPrediction() {
     return activePrediction;
 }
 
+function checkPrediction(lastClosed5m) {
+
+    if (!activePrediction) return;
+
+    if (activePrediction.status !== "Pending") return;
+
+    if (lastClosed5m.time < activePrediction.expiryTime) return;
+
+    if (activePrediction.signal === "BUY") {
+
+        activePrediction.status =
+            lastClosed5m.close > activePrediction.openPrice
+                ? "WIN"
+                : "LOSS";
+
+    }
+
+    else if (activePrediction.signal === "SELL") {
+
+        activePrediction.status =
+            lastClosed5m.close < activePrediction.openPrice
+                ? "WIN"
+                : "LOSS";
+
+    }
+
+}
+
 module.exports = {
     setPrediction,
-    getPrediction
+    getPrediction,
+    checkPrediction
 };
