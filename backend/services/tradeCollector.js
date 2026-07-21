59f328993b9603aec4b7cb36c4f620edcc97447e
Prasad Gagare <prasadgagare91@gmail.com>
2026-07-22 01:00:13 +0530

    Refactor processTrade function in tradeCollector.js
    Refactor processTrade function to improve readability and maintainability. Added calculations for aggressive buy/sell volumes and their ratios.

--- backend/services/tradeCollector.js
+++ backend/services/tradeCollector.js
@@ -1,6 +1,9 @@
 const marketState = require("../data/marketState");
 
-function processTrade(trade){
+
+const marketState = require("../data/marketState");
+
+function processTrade(trade) {
 
     const price = Number(trade.p);
 
@@ -12,37 +15,66 @@ function processTrade(trade){
 
     marketState.tradeCount++;
 
-    if(trade.m){
+    if (trade.m) {
 
+        // Seller Aggressive
         marketState.sellVolume += volume;
 
         marketState.delta -= volume;
 
         marketState.cvd -= volume;
 
-        if(volume > marketState.largestSell){
+        marketState.aggressiveSellVolume += volume;
 
-            marketState.largestSell = volume;
-
+        if (volume >= 50000) {
+            marketState.largeSellOrders++;
         }
 
-    }else{
+        if (volume > marketState.largestSell) {
+            marketState.largestSell = volume;
+        }
 
+    } else {
+
+        // Buyer Aggressive
         marketState.buyVolume += volume;
 
         marketState.delta += volume;
 
         marketState.cvd += volume;
 
-        if(volume > marketState.largestBuy){
+        marketState.aggressiveBuyVolume += volume;
 
+        if (volume >= 50000) {
+            marketState.largeBuyOrders++;
+        }
+
+        if (volume > marketState.largestBuy) {
             marketState.largestBuy = volume;
-
         }
 
     }
 
+    const total =
+        marketState.aggressiveBuyVolume +
+        marketState.aggressiveSellVolume;
+
+    marketState.aggressiveRatio =
+        total === 0
+            ? 50
+            : Number(
+                (
+                    marketState.aggressiveBuyVolume /
+                    total
+                ) * 100
+            ).toFixed(2);
+
 }
+
+module.exports = {
+    processTrade
+};
+
 
 module.exports = {
 
