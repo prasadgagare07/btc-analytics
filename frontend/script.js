2b76d9937b85452cabd5c7772bd56d1fa7bb7dd1
Prasad Gagare <prasadgagare91@gmail.com>
2026-07-21 12:40:25 +0530

    Add additional loading functions to refresh

--- frontend/script.js
+++ frontend/script.js
@@ -350,8 +350,19 @@ function startCountdown(expiryTime) {
 }
 
 async function refresh() {
+
     await loadStatus();
     await loadMarket();
+    await loadOrderBook();
+    await loadIndicators();
+    await loadPrediction();
+    await loadActivePredictions();
+    await loadAccuracy();
+    await loadOpenInterest();
+    await loadFunding();
+    await loadLiquidations();
+    await loadChart();
+
 }
 
 refresh();
